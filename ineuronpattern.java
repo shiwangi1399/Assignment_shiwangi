0a6be3d47686a78f134ddc015b86ad536efe24a4
shiwangi1399 <107622418+shiwangi1399@users.noreply.github.com>
2022-10-23 00:36:49 +0530

    Add files via upload

---
+++ ineuronpattern.java
@@ -0,0 +1,111 @@
+import java.util.*;
+public class ineuronpattern {
+	public static void main( String[] args)
+	 {
+		int n=10;
+					
+			for(int i=0;i<n; i++)
+			{
+				for(int j=0 ; j<n; j++)
+				{
+					if(j==(n-1)/2 || i==0||i==n-1)
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+						
+					}
+					
+				}
+				
+				
+				for(int j=0 ; j<n; j++)
+				{
+					if(j==0|| j==n-1 ||j==i) 
+			
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+						
+					}
+					
+				}
+				for(int j=0 ; j<n; j++)
+				{
+					if(j==0 || i==0 || i==n-1 ||i==(n-1)/2)
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+						
+					}
+					
+				}
+				for(int j=0 ; j<n; j++)
+				{
+					if(j==0 || i==n-1 || j==n-1 )
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+						
+					}
+					
+				}
+				for(int j=0 ; j<n; j++)
+				{
+					if(j==0 || i==0 && j< 3*n/4 || i==n/2 && j< 3*n/4 || j==3*n/4 && i<n/2 ||i-j==(n-1)/2)
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+						
+					}
+					
+				}
+				for(int j=0 ; j<n; j++)
+				{
+					if(i==0 && j>0 && j<n-1 ||j==0 && i>0 && i<n-1 || i==n-1 && j>0 && j<n-1 || j==n-1 && i>0 && i<n-1)
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+						
+					}
+					
+				}
+				for(int j=0 ; j<n; j++)
+				{
+					if(j==0 || j==n-1 ||i==j)
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+						
+					}
+					
+				}
+				
+				System.out.println();
+			}
+			
+
+		}
+
+	}
+
---
+++ numberpattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class numberpattern {
+ 
+ public static void main(String args[])
+ {
+	 int n=4,x=1;
+	 for(int i=0;i<n;i++)
+	 {
+		 for(int j=0;j<n;j++)
+		 {
+			 System.out.print(x);
+		 }
+		 System.out.println();
+		 x++;
+	 }
+ }
+}
